5afc6bc936439d0491a7cb492fa8cad6135fc294
X.Heart <xiaoxin.wang@xinhuajindian.com>
2018-06-29 14:25:22 +0800

    add city-search

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -11,7 +11,7 @@
 
 <script>
 export default {
-  name: 'HomeHeader',
+  name: 'CityHeader',
   props: {
     city: String
   }
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch',
+  props: {
+    city: String
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '~styles/varibles.less';
+.search{
+  padding: 0 .1rem;
+  height: .72rem;
+  background: @bgColor;
+  .search-input{
+    width: 100%;
+    height: .62rem;
+    line-height: .62rem;
+    text-align: center;
+    border-radius: .06rem;
+    color: #666;
+    padding: 0 0.1rem;
+    box-sizing: border-box;
+  }
+}
+</style>
